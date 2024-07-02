06cb04c5d227181c99fecd3c576504c6e7276a83
Mateus Berlitz <wobetoberlitz@gmail.com>
2024-07-02 16:26:39 -0300

    fix unused components

--- src/components/SimpleLoader/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Spinner } from "@chakra-ui/react";
-
-export function SimpleLoader(){
-    return(
-        <Flex id="simpleLoading" top={0} left={0} right={0} bottom={0} bg="white" pos={"fixed"} justifyContent={"center"} alignItems={"center"} zIndex={9999999999991}>
-            <Spinner />
-        </Flex>
-    )
-}
